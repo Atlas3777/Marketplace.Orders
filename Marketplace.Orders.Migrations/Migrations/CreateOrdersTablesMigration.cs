4df0760deb984c4960f5a3a6921c1560ffdb5773
artem <artem.af.2005@gmail.com>
2026-06-22 19:08:48 +0500

    fix: migration

--- Marketplace.Orders.Migrations/Migrations/CreateOrdersTablesMigration.cs
+++ Marketplace.Orders.Migrations/Migrations/CreateOrdersTablesMigration.cs
@@ -2,39 +2,29 @@ using FluentMigrator;
 
 namespace Marketplace.Orders.Migrations.Migrations;
 
-[Migration(202606220033, "Create Orders Tables")]
+[Migration(202606221905, "Create Orders Tables")]
 public class CreateOrdersTablesMigration : Migration
 {
     public override void Down()
     {
-        if (Schema.Table("order_items").Exists())
-            Delete.Table("order_items");
-        if (Schema.Table("orders").Exists())
-            Delete.Table("orders");
+        Delete.Table("order_items");
+        Delete.Table("orders");
     }
 
     public override void Up()
     {
-        if (!Schema.Table("orders").Exists())
-        {
-            Create.Table("orders")
-                .WithColumn("id").AsGuid().PrimaryKey()
-                .WithColumn("userid").AsGuid().NotNullable()
-                .WithColumn("totalprice").AsDecimal().NotNullable()
-                .WithColumn("status").AsInt32().NotNullable().WithDefaultValue(1) 
-                .WithColumn("createdat").AsDateTimeOffset().NotNullable()
-                .WithDefault(SystemMethods.CurrentUTCDateTime);
-        }
+        Create.Table("orders")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("userid").AsGuid().NotNullable()
+            .WithColumn("totalprice").AsDecimal().NotNullable()
+            .WithColumn("status").AsInt32().NotNullable().WithDefaultValue(1) 
+            .WithColumn("createdat").AsDateTimeOffset().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime);
 
-        if (!Schema.Table("order_items").Exists())
-        {
-            Create.Table("order_items")
-                .WithColumn("id").AsGuid().PrimaryKey()
-                .WithColumn("orderid").AsGuid().NotNullable()
-                .ForeignKey("orders", "id")
-                .WithColumn("productid").AsGuid().NotNullable()
-                .WithColumn("quantity").AsInt32().NotNullable()
-                .WithColumn("price").AsDecimal().NotNullable();
-        }
+        Create.Table("order_items")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("orderid").AsGuid().NotNullable().ForeignKey("orders", "id")
+            .WithColumn("productid").AsGuid().NotNullable()
+            .WithColumn("quantity").AsInt32().NotNullable()
+            .WithColumn("price").AsDecimal().NotNullable();
     }
 }
